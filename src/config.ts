7b17d07efc06cf33ded0b1c265e23dd80e4dd268
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-09-19 00:10:29 -0600

    Change CV, index and cv pages

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Bryan Quezada - Personal Portfolio';
-export const SITE_DESCRIPTION = 'Portfolio personal de Bryan Quezada, Ingeniero en Sistemas y Desarrollador Web';
+export const SITE_DESCRIPTION = 'Bryan Quezada\'s personal portfolio, Systems Engineer and Web Developer';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
